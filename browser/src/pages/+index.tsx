eaca02ec7e2a4ef4da8a54dfb847a1bb3ebffcdd
Ben Brady <benbrady04.bb@gmail.com>
2025-03-09 23:25:45 +0000

    fix: blocks count

--- browser/src/pages/+index.tsx
+++ browser/src/pages/+index.tsx
@@ -15,7 +15,7 @@ const ListingPage: FC = () => {
     return (
         <div className="flex flex-col gap-4 max-w-128 w-full">
             <div className="flex justify-end items-center gap-4 w-full">
-                <span className="font-minecraft">Blocks {blocks.size}</span>
+                <span className="font-minecraft">{Object.values(blocks).length} Blocks</span>
                 <Button onClick={onPingAll}>Refresh All</Button>
             </div>
             {computers.map((computer) => (
