6c8bee0632fcf1a4acf16b56a8e6352c8221e379
Ben Brady <benbrady04.bb@gmail.com>
2025-03-11 04:35:28 +0000

    perf(isBlockOccluding): removed cache

--- browser/src/lib/minecraft/occluding.ts
+++ browser/src/lib/minecraft/occluding.ts
@@ -1,18 +1,10 @@
 import { isFlower } from "@/components/extensive/Canvas/Block/FlowerMesh";
 import { isLiquid } from "@/components/extensive/Canvas/Block/LiquidMesh";
 
-const cache = new Map<string, boolean>();
 export const isBlockOccluding = (name: string): boolean => {
-    const cached = cache.get(name);
-    if (cached !== undefined) return cached;
-
-    const value = (() => {
-        if (name === "minecraft:air") return false;
-        if (name.includes("turtle")) return false;
-        if (isLiquid(name)) return false;
-        if (isFlower(name)) return false;
-        return true;
-    })();
-
-    return value;
+    if (name === "minecraft:air") return false;
+    if (name.includes("turtle")) return false;
+    if (isLiquid(name)) return false;
+    if (isFlower(name)) return false;
+    return true;
 };
